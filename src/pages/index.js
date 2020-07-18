69d029718e457a7e747ee18022052b1f85fb6400
Maciej Burda <maciej.burda@me.com>
2020-07-18 17:04:06 +0200

    Clean up

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,11 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Tu powstaje strona internetowa domu opieki Srebrny Dom w miejscowości Swornegacie.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <Link to="https://www.facebook.com/SrebrnyDomSwornegacie/">Strona na Facebooku</Link> <br />
   </Layout>
 )
 
