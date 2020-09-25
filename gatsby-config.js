daa137bc9f45c2d189ef410c95aa83c978d9d2b2
Maciej Burda <maciej.burda@me.com>
2020-09-25 08:49:51 +0200

    GA config

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,10 +74,27 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: config.googleAnalyticsId,
-        header: true,
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          config.googleAnalyticsId, // Google Analytics / GA
+          "AW-994571338'", // Google Ads / Adwords / AW
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+        },
       },
     },
     {
