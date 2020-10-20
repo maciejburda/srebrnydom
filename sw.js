4fd08f13e3e432d303c868f2e578f090c5f5a16a
Maciej Burda <maciej.burda@me.com>
2020-10-20 09:06:25 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-4379c1364a5eab72d2d8.js"
+    "url": "webpack-runtime-f80ccadfbaacd5902196.js"
   },
   {
     "url": "framework-a38040c4749cf445737f.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "24cd799419f5340ecd968a3d9090c46e"
+    "revision": "53931e234a5724dfaed97523bec960d6"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-5a1ee105f05b3144b807.js"
